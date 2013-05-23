ff50c0f9385da6e1fe4457b69eca4f0d3826a0b4
linet <glinet1002@gmail.com>
2013-05-23 12:14:06 +0800

    create  ruby app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DummyApp::Application.config.secret_token = '4c6151bd4ba313a3170fa1b23ec4293ea3bf695a203690b685b365ec499dd2734081bb832b7edfa83c0de2f528a4f35198d3893ee69302108585678760e28d6b'
