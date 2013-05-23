716d2498531fe35c45e50acba11ebadd284131ad
linet <glinet1002@gmail.com>
2013-05-23 12:51:16 +0800

    create

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-DummyApp::Application.config.secret_token = '4c6151bd4ba313a3170fa1b23ec4293ea3bf695a203690b685b365ec499dd2734081bb832b7edfa83c0de2f528a4f35198d3893ee69302108585678760e28d6b'
+DummyApp::Application.config.secret_token = 'b1e45690658575640fd09374a566dcd24c2105004bcf002fdfab7e80b7f379eb574f16cae17b0aa4d0a54a8c0f61beb014767d79a4ebc1a5db3e8e4114d93d91'
